0139a53d686d8b935e4fb53ab39f7121ad2490b3
dmda0418 <dmytro.davydenko@netcracker.com>
2019-09-18 18:47:16 +0300

    Added Animation component to configuration form

--- src/app/modules/pages/playground/components/animation-examples/ag-animation-examples.component.ts
+++ src/app/modules/pages/playground/components/animation-examples/ag-animation-examples.component.ts
@@ -1,28 +1,52 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, forwardRef, OnInit } from '@angular/core';
+
 import { AgAnimationEnum } from "../../../../../enums";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 
 @Component({
   selector: 'ag-animation-examples',
   templateUrl: './ag-animation-examples.component.html',
-  styleUrls: ['./ag-animation-examples.component.less']
+  styleUrls: ['./ag-animation-examples.component.less'],
+  providers: [
+  {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => AgAnimationExamplesComponent),
+    multi: true
+  }
+]
 })
-export class AgAnimationExamplesComponent implements OnInit {
-
-  @Input()
-  formControlName: string;
-
-  @Output()
-  onAnimationSelected = new EventEmitter();
+export class AgAnimationExamplesComponent implements OnInit, ControlValueAccessor  {
 
   animationList = Object.values(AgAnimationEnum);
+  hoveredIndex: number;
+  selectedAnimation: string;
 
-  setAnimation(selected: string): void {
-    this.onAnimationSelected.emit(selected);
+  setAnimation(selectedKeyframe: string): void {
+    this.selectedAnimation = selectedKeyframe;
+    this.propagateChange(this.selectedAnimation);
   }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  propagateChange = (_: any) => {};
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+  }
+
+  writeValue(keyframe: string): void {
+    if (keyframe) {
+      this.selectedAnimation = keyframe;
+    }
   }
 
 }
